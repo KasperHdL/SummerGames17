b60c158530ec8d9e55e294285de427950c97c641
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-08-17 20:49:23 +0200

    Light Swarming

--- Assets/Scripts/Swarm/SwarmClient.cs
+++ Assets/Scripts/Swarm/SwarmClient.cs
@@ -6,14 +6,22 @@ public class SwarmClient : MonoBehaviour {
 
     public bool debug;
 
+    public float move_force;
+
+    public Vector3 direction;
+
+    [Header("Effect decay")]
+    public Vector3 effect_direction;
+    public float effect_weight;
+
+    public AnimationCurve effect_decay;
+    private float last_effect_time;
+
+    [Header("Surroundings")]
     public float range;
     public float forward_range;
 
-    public float move_force;
-    public float swarm_force;
-    public float swarm_decay;
 
-    public float swarm_velo_minimum;
 
 
     private Rigidbody body;
@@ -24,27 +32,53 @@ public class SwarmClient : MonoBehaviour {
 
     void Update(){
 
-        RaycastHit[] hits = Physics.CapsuleCastAll(transform.position, transform.position + Vector3.up, range, transform.forward, forward_range);
+        float decay = effect_decay.Evaluate(Time.time - last_effect_time);
+        if(debug)print("Decay " + decay);
 
-        for(int i = 0; i < hits.Length; i++){
-            if(hits[i].transform == transform) continue;
-
-            SwarmClient client = hits[i].collider.GetComponent<SwarmClient>();
+        Vector3 surroundings = CalcSurroundEffect();
 
 
-            if(hits[i].rigidbody.velocity.magnitude > swarm_velo_minimum){
-                body.AddForce(hits[i].rigidbody.velocity.normalized * swarm_force * Time.deltaTime * swarm_decay);
 
-                if(debug){
-                    Debug.DrawRay(hits[i].transform.position, hits[i].rigidbody.velocity, Color.green);
-                    Debug.DrawRay(transform.position, body.velocity, Color.green);
-                }
+        direction = (surroundings/effect_weight) * (1-decay) + effect_direction * decay;
+        body.AddForce(direction * Time.deltaTime * move_force);
 
-            }else if(debug){
-                Debug.DrawRay(hits[i].transform.position, hits[i].rigidbody.velocity, Color.red);
-                print(i + ": " + hits[i].rigidbody.velocity);
+        Debug.DrawRay(transform.position, direction.normalized, Color.green);
+        Debug.DrawRay(transform.position, effect_direction, Color.yellow);
+        Debug.DrawRay(transform.position, surroundings, Color.red);
+    }
 
-            }
-        }
+    public void Effect(Vector3 direction){
+        last_effect_time = Time.time;
+
+        effect_direction = direction;
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+    }
+
+    public Vector3 CalcSurroundEffect(){
+
+        Vector3 dir = Vector3.zero;
+
+        RaycastHit[] hits = Physics.CapsuleCastAll(transform.position, transform.position + Vector3.up, range, body.velocity.normalized, forward_range); 
+
+        for(int i = 0; i < hits.Length; i++){ 
+            if(hits[i].transform == transform) continue; 
+
+            SwarmClient client = hits[i].collider.GetComponent<SwarmClient>(); 
+
+            dir += hits[i].rigidbody.velocity;
+        } 
+
+        return dir.normalized;
+
+
+    }
+
+    public struct EffectInfo{
+        int id;
+        float timestamp;
+        AnimationCurve decay;
+        float force;
+        Vector3 direction;
     }
 }
--- Assets/Scripts/Swarm/SwarmEffect.cs
+++ Assets/Scripts/Swarm/SwarmEffect.cs
@@ -4,6 +4,78 @@ using UnityEngine;
 
 public class SwarmEffect : MonoBehaviour {
 
+    static int id_counter = 0;
+    int id;
+
+    public enum Type{
+        Direction,
+        Push,
+        Pull
+    };
+
+    public Type type;
+
+    [Range(0,1)]public float persuasion;
+
+    public Vector3 direction;
+
+    [Header("Trigger Type")]
+    public bool on_enter;
+    public bool on_stay;
+    public bool on_exit;
+
+    void Start(){
+        id = id_counter++;
+    }
+
+    public void OnTriggerEnter(Collider coll){
+        if(!on_enter)return;
+
+        SwarmClient client = coll.GetComponent<SwarmClient>();
+        if(client == null) return;
+        client.Effect(CalcEffect(client));
+
+    }
+
+    public void OnTriggerStay(Collider coll){
+        if(!on_stay)return;
+
+        SwarmClient client = coll.GetComponent<SwarmClient>();
+        if(client == null) return;
+        client.Effect(CalcEffect(client));
+
+    }
+
+    public void OnTriggerExit(Collider coll){
+        if(!on_exit)return;
+
+        SwarmClient client = coll.GetComponent<SwarmClient>();
+
+        if(client == null) return;
+
+        client.Effect(CalcEffect(client));
+
+    }
+
+    public Vector3 CalcEffect(SwarmClient client){
+
+        Vector3 v = Vector3.zero;
+
+        switch(type){
+            case Type.Direction:{
+                v = direction;
+            }break;
+            case Type.Push:{
+                v = client.transform.position - transform.position;
+
+            }break;
+            case Type.Pull:{
+                v =  transform.position - client.transform.position;
+            }break;
+        }
 
 
+        v = Vector3.Lerp(client.effect_direction, v, persuasion);
+        return v;
+    }
 }
