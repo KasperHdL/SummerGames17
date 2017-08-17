4cb16cdbe3eb8453a45db0176ed246040cd748ab
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-08-17 18:00:42 +0200

    Moving Guide

---
+++ Assets/Scripts/Guide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guide : MonoBehaviour {
+
+    public float move_force;
+
+
+    private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        body.AddForce(move * move_force * Time.deltaTime);
+		
+	}
+}
