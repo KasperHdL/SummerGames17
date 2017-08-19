a6f69d1fc3b9fbb68e1968448e6d5df0cf542ff7
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-08-19 15:18:25 +0200

    The Worst Camera period.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,50 +5,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     public Transform target;
+    public Vector3 offset;
 
-    public float min_distance;
-    public float max_distance;
-
-    public float max_move;
-    public float move_force;
-
-
+    
     private Rigidbody body;
-    private Rigidbody target_body;
+
 	void Start () {
-        body = GetComponent<Rigidbody>();
-        target_body = target.GetComponent<Rigidbody>();
+        body = target.GetComponent<Rigidbody>();
 
 		
 	}
 	
 	void Update () {
 
+        transform.position = target.position - offset;
+
         //rotation
         transform.LookAt(target);
-
-        //move
-
-        Vector3 delta = target.position - transform.position;
-        delta.y = 0;
-
-        Vector3 move = Vector3.zero;
-        if(delta.magnitude < min_distance){
-            move = -delta.normalized * (min_distance - delta.magnitude) * move_force;
-
-        }
-
-        if(delta.magnitude > max_distance){
-            move = delta.normalized * (delta.magnitude - max_distance) * move_force;
-        }
-
-        if(move.magnitude > max_move){
-            move = move.normalized * max_move;
-        }
-
-
-        body.AddForce(move * Time.deltaTime);
-
 
 		
 	}
