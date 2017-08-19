8b91789c3f2ebb80407997065ee73ede71ea4d06
Frans Peter Larsen <larsen.frans@gmail.com>
2017-08-19 20:02:37 +0200

    Random Material cs, Fountain prefab w/ mat, tex, model

---
+++ Assets/AddRandomMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRandomMaterial : MonoBehaviour {
+    public Material[] materials;
+
+    [ContextMenu("Add Random Materials")]
+    void AddMats()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+        }
+    }
+}
