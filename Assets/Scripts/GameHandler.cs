e95841f474293f02996064f96a1dda5ee2e90bdd
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-08-20 13:01:52 +0200

    Timer

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour {
+
+
+    public bool timer_started = false;
+    public float max_time;
+    private float start_time;
+    public Text timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer_started){
+            timer.text = Mathf.RoundToInt(max_time - (Time.time - start_time)) + "";
+        }
+	}
+
+
+    public void Start_Timer(){
+        start_time = Time.time;
+        timer_started = true;
+
+    }
+}
