ef5567409d95b536bb81c6f127d642cbbac97296
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-08-20 12:30:42 +0200

    Bus Effect

---
+++ Assets/Scripts/EnterBus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterBus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll){
+        if(coll.GetComponent<SwarmClient>() == null) return;
+
+        //count excitement
+
+        Destroy(coll.gameObject);
+
+
+
+    }
+}
